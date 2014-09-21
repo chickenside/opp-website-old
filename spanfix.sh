9f627a80cbd90c25535361c26ccc5769ca0b0465
chickenside <chickenside@gmail.com>
2014-09-20 20:04:02 -0500

    bla

---
+++ spanfix.sh
@@ -0,0 +1,14 @@
+$ cat work.html |grep "Prowl</span>"
+                    <span class="light">Prowl</span>
+                            <span class="light">Prowl</span>around the Ogden Park neighborhood in the Baton Rouge Mid City Arts District.</p>
+                    <span class="light">Prowl</span>is a neighborhood art festival in Mid City Baton Rouge. Come
+                    <span class="light">Prowl</span>about our neighborhood September, 27 from 1-6pm.</p>
+                    <span class="light">Prowl</span>is that other groovy thing!</p>
+                    <span class="light">Prowl</span>about their concerns.</p>
+                    <span class="light">Prowl</span>around with us this September, 27 from 1-6pm.</p>
+                        <span class="light">Prowl</span>!
+                        <span class="light">Prowl</span>? Well feast your greasy peepers on these groovy samples by the artists.
+                <span class="light">Prowl</span>
+
+
+sed 's/
